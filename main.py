7f86c276dd378ff9146b67a2181dabd5ef16e284
Louis Parizeau <lgparizeau@gmail.com>
2025-02-06 15:53:47 -0500

    dobot queue can only handle a few commands

--- main.py
+++ main.py
@@ -8,24 +8,23 @@ CON_STR = {
 
 def send_commands(gcode):
     for command in gcode:
+        dType.SetQueuedCmdClear(api)
+        dType.SetQueuedCmdStartExec(api)
         lastIndex = dType.SetPTPCmd(api, 2, command.X, command.Y, command.Z, 0, 1)
+        while lastIndex > dType.GetQueuedCmdCurrentIndex(api)[0]:
+            dType.dSleep(100)
+        dType.SetQueuedCmdStopExec(api)
     return lastIndex[0]
     
 def execute_commands(gcode):
-    dType.SetQueuedCmdClear(api)
     lastIndex = send_commands(gcode)
-    dType.SetQueuedCmdStartExec(api)
-    # dType.SetPTPCoordinateParamsEx(api,200,1000,200,1000,1)
-    while lastIndex > dType.GetQueuedCmdCurrentIndex(api)[0]:
-        dType.dSleep(100)
-    dType.SetQueuedCmdStopExec(api)
 
 if __name__ == "__main__":
     api = dType.load()
     state = dType.ConnectDobot(api, "", 115200)[0]
 
     if (state == dType.DobotConnect.DobotConnect_NoError):
-        parser = GCodeParser("data/sample.gcode")
+        parser = GCodeParser("data/butterfly.svg")
         parser.transform()
         parser.generate_gcode()
         execute_commands(parser.gcode)
